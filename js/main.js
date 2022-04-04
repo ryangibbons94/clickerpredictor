79fb6a1e7471dd0e3df5b749a1f13cf5c02e0f7b
ryangibbons94 <96315276+ryangibbons94@users.noreply.github.com>
2022-04-03 20:17:30 -0400

    transfered all files from codepen

--- js/main.js
+++ js/main.js
@@ -1,1 +1,87 @@
 
+    let score; // to store the current score
+    //NEED TO ADD AN EVENT LISTENER THAT CHANGES THE DURATION EVERYTIME THE INPUT IS CHANGED
+        let select = document.querySelector('select')
+        // var duration = Number(select.value);
+        let duration
+        let startTime; // start time
+        let ended = true; // boolean indicating if game is ended
+        // we get DOM References for some HTML elements
+        let timerTxt = document.getElementById("timer");
+        let scoreTxt = document.getElementById("score");
+        let clicksTxt = document.getElementById("clicks");
+        let startBtn = document.getElementById("start");
+        let clickArea = document.getElementById("clickarea");
+        let prediction = document.getElementById("prediction");
+        let gameCount = 0
+        // we define two functions for showing or hiding a HTML element
+        let show = function(elem) {
+          elem.style.display = 'inline';
+        };
+        let hide = function(elem) {
+          elem.style.display = 'none';
+        };
+        // Method called when the game starts
+        function startGame() {
+          duration = Number(select.value);
+          hide(startBtn);
+          score = 0;
+          ended = false;
+          // we get start time
+          startTime = new Date().getTime();
+          // we create a timer with the setInterval method
+          let timerId = setInterval(function() {
+            let total = (new Date().getTime() - startTime) / 1000;
+            // while total lower than duration, we update timer and the clicks by seconds
+            if (total < duration) {
+              timerTxt.textContent = total.toFixed(3);
+              clicksTxt.textContent = (score / total).toFixed(2);
+            } else {
+              // otherwise, game is ended, we clear interval and we set game as ended
+              ended = true;
+              clearInterval(timerId);
+              // we call the end game method
+              endGame();
+            }
+          }, 1);
+      }
+      // end game method
+      function endGame() {
+        // we write final stats
+        let clicksBySeconds = (score / duration).toFixed(2);
+        timerTxt.textContent = duration.toFixed(3);
+        clicksTxt.textContent = clicksBySeconds;
+        gameCount++
+        // we show start button to play another game
+        show(startBtn);
+        // we display result to the user in delayed mode 
+        //to update DOM elements just before the alert
+        setTimeout(function() {
+          alert('You made ' + score + ' clicks in ' + duration + 
+          ' seconds. It is ' + clicksBySeconds + 
+          ' clicks per second. Try again!');
+        }, 10);
+      }
+      // we set a click event listener on the start button
+      startBtn.addEventListener("click", function(e) {
+        startGame();
+      });
+      // we add a click event listener on the click area div to update the score when the user will click
+      clickArea.addEventListener("click", function(e) {
+        if (!ended) {
+          score++;
+          scoreTxt.textContent = score;
+        }
+      });
+    
+    //add prediction after first game played
+    
+    select.addEventListener('change', predict)
+    
+    function predict(){
+      let newPrediction 
+      if (gameCount > 0){
+        newPrediction = (score * select.value)/duration
+        prediction.textContent = `Based on your previous score, in ${select.value} seconds you should get ${newPrediction}`
+      }
+    }
